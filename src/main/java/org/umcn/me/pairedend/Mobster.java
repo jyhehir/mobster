24bc090305de255eb7ea2c53554cca1dba1637db
Djie Twjan Thung <DjieTjwan.Thung@radboudumc.nl>
2015-02-02 13:58:38 +0000

    Info test commit
    git-svn-id: svn://russell/MobileInsertions/trunk@8932 b59cde58-9791-4221-a6f8-7311ddf0e4ec

--- src/main/java/org/umcn/me/pairedend/Mobster.java
+++ src/main/java/org/umcn/me/pairedend/Mobster.java
@@ -20,7 +20,7 @@ public class Mobster {
 	public static String inFile = null;
 	public static String outFile = null;
 	public static String sampleName = null;
-	public static final String VERSION = "0.1.6-OtherMEsRebuild";
+	public static final String VERSION = "0.1.6-Test";
 	
 	public static void main(String[] args) {
 		
