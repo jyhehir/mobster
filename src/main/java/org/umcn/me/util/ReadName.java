00600336cd9e358e1e5e7dce367b605c89fe4f8f
Djie Twjan Thung <DjieTjwan.Thung@radboudumc.nl>
2015-07-28 07:12:45 +0000

    Adjustments to add annotations to readname
    git-svn-id: svn://russell/MobileInsertions/trunk@10931 b59cde58-9791-4221-a6f8-7311ddf0e4ec

--- src/main/java/org/umcn/me/util/ReadName.java
+++ src/main/java/org/umcn/me/util/ReadName.java
@@ -2,16 +2,29 @@ package org.umcn.me.util;
 
 import net.sf.samtools.SAMRecord;
 
+import java.util.List;
+
+import org.umcn.me.tabix.RefGeneAnnotation;
+import org.umcn.me.tabix.RepMaskAnnotation;
+
 public class ReadName {
 	
 	public final String readName;
 	public final String reference;
 	public final int start;
 	public final int end;
+	public final boolean isMapped;
 	
 	public final String mateReference;
 	public final int mateStart;
 	public final int mateEnd;
+	public final boolean mateIsMapped;
+	
+	private List<RepMaskAnnotation> repmask;
+	private List<RefGeneAnnotation> refgene;
+	
+	private List<RepMaskAnnotation> mateRepMask;
+	private List<RefGeneAnnotation> mateRefGene;
 	
 	private ReadNameOption option;
 	
@@ -23,10 +36,12 @@ public class ReadName {
 		this.reference = option.prefixReference + record.getReferenceName();
 		this.start = record.getAlignmentStart();
 		this.end = record.getAlignmentEnd();
+		this.isMapped = ! record.getReadUnmappedFlag();
 		
 		this.mateReference = option.prefixReference + record.getMateReferenceName();
 		this.mateStart = record.getMateAlignmentStart();
 		this.mateEnd = record.getMateAlignmentStart() + record.getReadLength() - 1;
+		this.mateIsMapped = ! record.getMateUnmappedFlag();
 
 	}
 	
@@ -35,14 +50,53 @@ public class ReadName {
 		return name.substring(removePrefix, name.length());
 	}
 	
-	
 	public String toString(){
 		if (!this.option.addRegion){
 			return this.readName;
 		}else{
-			return this.readName + "\t" + this.reference + "\t" + 
-					this.start + "\t" + this.end + "\t" + this.mateReference + "\t" + this.mateStart + "\t" + this.mateEnd;
+			return this.readName + "\t" + this.reference + "\t" + this.start + "\t" + this.end + "\t" + this.isMapped + "\t" +
+					this.mateReference + "\t" + this.mateStart + "\t" + this.mateEnd + "\t" + this.mateIsMapped;
 		}
+	}
+	
+	public String toPositionString(){
+		return this.reference + ":" + this.start + "-" + this.end;
+	}
+	
+	public String mateToPositionString(){
+		return this.mateReference + ":" + this.mateStart + "-" + this.mateEnd;
+	}
+	
+	public List<RepMaskAnnotation> getRepMaskAnnotation(){
+		return this.repmask;
+	}
+	
+	public List<RefGeneAnnotation> getRefGeneAnnotation(){
+		return this.refgene;
+	}
+	
+	public void setRepMaskAnnotation(List<RepMaskAnnotation> repmaskAnnotations){
+		this.repmask = repmaskAnnotations;
+	}
+	
+	public void setRefGeneAnnotation(List<RefGeneAnnotation> refGeneAnnotations){
+		this.refgene = refGeneAnnotations;
+	}
+	
+	public List<RepMaskAnnotation> getMateRepMaskAnnotation(){
+		return this.mateRepMask;
+	}
+	
+	public List<RefGeneAnnotation> getMateRefGeneAnnotation(){
+		return this.mateRefGene;
+	}
+	
+	public void setMateRepMaskAnnotation(List<RepMaskAnnotation> repmaskAnnotations){
+		this.mateRepMask = repmaskAnnotations;
+	}
+	
+	public void setMateRefGeneAnnotation(List<RefGeneAnnotation> refGeneAnnotations){
+		this.mateRefGene = refGeneAnnotations;
 	}
 	
 	
